c8c5dff1bd88bd02482694693383d58d36dcd321
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-02-25 18:26:57 +0700

    Update

--- src/com/franktran/Main.java
+++ src/com/franktran/Main.java
@@ -1,15 +1,20 @@
 package com.franktran;
 
 import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main extends Application {
+
+    private List<String> programs = new ArrayList();
 
     public static void main(String[] args) {
 	    launch(args);
@@ -17,35 +22,44 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Label label = new Label("Enter your age:");
-        GridPane.setConstraints(label, 0, 0);
+        CheckBox htmlCheckBox = new CheckBox("HTML");
+        htmlCheckBox.setSelected(true);
+        htmlCheckBox.setOnAction(actionEvent -> change(htmlCheckBox));
+        htmlCheckBox.selectedProperty().addListener(
+            (ObservableValue<? extends Boolean> ov, Boolean old_val, Boolean new_val) -> change(htmlCheckBox)
+        );
 
-        TextField input = new TextField();
-        GridPane.setConstraints(input, 1, 0);
+        CheckBox cssCheckBox = new CheckBox("CSS");
+        cssCheckBox.selectedProperty().addListener(
+            (ObservableValue<? extends Boolean> ov, Boolean old_val, Boolean new_val) -> change(cssCheckBox)
+        );
 
-        Button button = new Button("Check Age");
-        GridPane.setConstraints(button, 1, 1);
-        button.setOnAction(actionEvent -> isInteger(input));
+        CheckBox jsCheckBox = new CheckBox("JAVASCRIPT");
+        jsCheckBox.selectedProperty().addListener(
+                (ObservableValue<? extends Boolean> ov, Boolean old_val, Boolean new_val) -> change(jsCheckBox)
+        );
 
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10, 10, 10, 10));
-        grid.setVgap(10);
-        grid.setHgap(8);
-        grid.getChildren().addAll(label, input, button);
+        Button button = new Button("Learn Now");
+        button.setOnAction(actionEvent -> System.out.println(String.format("You choose: %s", programs)));
 
-        Scene scene = new Scene(grid, 300, 200);
+        VBox vBox = new VBox(10);
+        vBox.setPadding(new Insets(10, 10, 10, 10));
+        vBox.getChildren().addAll(htmlCheckBox, cssCheckBox, jsCheckBox, button);
+
+        Scene scene = new Scene(vBox, 300, 200);
 
         stage.setScene(scene);
-        stage.setTitle("Validate Input");
+        stage.setTitle("Checkbox");
         stage.show();
     }
 
-    private void isInteger(TextField input) {
-        try {
-            int year = Integer.parseInt(input.getText());
-            System.out.println(String.format("You are %d years old", year));
-        } catch (NumberFormatException e) {
-            System.out.println(String.format("Error: %s is not a number", input.getText()));
+    private void change(CheckBox checkBox) {
+        if (checkBox.isSelected()) {
+            programs.add(checkBox.getText());
+        } else {
+            programs.remove(checkBox.getText());
         }
     }
+
+
 }
