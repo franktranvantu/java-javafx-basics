feff96e276641a61301abf28b4a27e98b64402c9
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-02-25 17:38:11 +0700

    Update

--- src/com/franktran/Main.java
+++ src/com/franktran/Main.java
@@ -1,9 +1,13 @@
 package com.franktran;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -16,25 +20,30 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Button fileButton = new Button("File");
-        Button editButton = new Button("Edit");
-        Button viewButton = new Button("View");
+        Label nameLabel = new Label("Username:");
+        GridPane.setConstraints(nameLabel, 0, 0);
 
-        Button studentButton = new Button("Students");
-        Button courseButton = new Button("Courses");
-        Button scheduleButton = new Button("Schedules");
+        TextField nameInput = new TextField();
+        nameInput.setPromptText("Username here...");
+        GridPane.setConstraints(nameInput, 1,0);
 
-        HBox topMenu = new HBox();
-        topMenu.getChildren().addAll(fileButton, editButton, viewButton);
+        Label passwordLabel = new Label("Password:");
+        GridPane.setConstraints(passwordLabel, 0, 1);
 
-        VBox leftMenu = new VBox();
-        leftMenu.getChildren().addAll(studentButton, courseButton, scheduleButton);
+        TextField passwordInput = new TextField();
+        passwordInput.setPromptText("Password here...");
+        GridPane.setConstraints(passwordInput, 1,1);
 
-        BorderPane layout = new BorderPane();
-        layout.setTop(topMenu);
-        layout.setLeft(leftMenu);
+        Button loginButton = new Button("Log In");
+        GridPane.setConstraints(loginButton, 1, 2);
 
-        Scene scene = new Scene(layout, 300, 250);
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(5);
+        grid.setHgap(5);
+        grid.getChildren().addAll(nameLabel, nameInput, passwordLabel, passwordInput, loginButton);
+
+        Scene scene = new Scene(grid, 300, 200);
 
         stage.setScene(scene);
         stage.setTitle("Embedding layouts");
