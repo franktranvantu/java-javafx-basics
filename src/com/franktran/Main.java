a39a2ac3c29fcd5a0525804cfb3e432cc55da6c2
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-02-25 17:06:33 +0700

    Update

--- src/com/franktran/Main.java
+++ src/com/franktran/Main.java
@@ -1,13 +1,9 @@
 package com.franktran;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -20,18 +16,27 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Button button = new Button("Click me!");
-        button.setOnAction(actionEvent -> {
-            boolean answer = ConfirmBox.display("Confirm change", "Are you sure you want to send naked pics?");
-            System.out.println(answer);
-        });
+        Button button = new Button("Close program");
+        button.setOnAction(actionEvent -> closeProgram());
 
         StackPane layout = new StackPane(button);
 
         Scene scene = new Scene(layout, 300, 250);
 
-        stage.setTitle("Create alert boxes");
-        stage.setScene(scene);
-        stage.show();
+        window = stage;
+        window.setOnCloseRequest(actionEvent -> {
+            actionEvent.consume();
+            closeProgram();
+        });
+        window.setTitle("Closing the Program Properly");
+        window.setScene(scene);
+        window.show();
+    }
+
+    private void closeProgram() {
+        boolean answer = ConfirmBox.display("Confirm change", "Sure you want to exit?");
+        if (answer) {
+            window.close();
+        }
     }
 }
