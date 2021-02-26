4ec79935249e57cd5e44ae92987d9040501ce950
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-02-26 14:20:22 +0700

    Update

--- src/com/franktran/Main.java
+++ src/com/franktran/Main.java
@@ -4,13 +4,11 @@ import javafx.application.Application;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
