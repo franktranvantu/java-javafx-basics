60b5a6095694af7b4111a12cd141d4348ba41dc5
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-02-26 09:45:26 +0700

    Update

--- src/com/franktran/Main.java
+++ src/com/franktran/Main.java
@@ -4,15 +4,13 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     private Stage window;
-    private Scene scene1, scene2;
 
     public static void main(String[] args) {
 	    launch(args);
@@ -22,23 +20,21 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         window = stage;
 
-        Label label1 = new Label("The first scene");
-        Button button1 = new Button("Go to second scene");
-        button1.setOnAction(actionEvent -> window.setScene(scene2));
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
+        choiceBox.getItems().addAll("HTML", "CSS", "JAVASCRIPT");
+        choiceBox.setValue("HTML");
 
-        VBox layout1 = new VBox(20);
-        layout1.setAlignment(Pos.CENTER);
-        layout1.getChildren().addAll(label1, button1);
-        scene1 = new Scene(layout1, 300, 200);
+        Button button = new Button("Click me");
+        button.setOnAction(actionEvent -> System.out.println(choiceBox.getValue()));
 
-        Button button2 = new Button("Go to first scene");
-        button2.setOnAction(actionEvent -> window.setScene(scene1));
+        VBox layout = new VBox(20);
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(choiceBox, button);
 
-        StackPane layout2 = new StackPane(button2);
-        scene2 = new Scene(layout2, 600, 300);
+        Scene scene = new Scene(layout, 300, 200);
 
-        window.setTitle("Switching Scenes");
-        window.setScene(scene1);
+        window.setTitle("Dropdown menu");
+        window.setScene(scene);
         window.show();
     }
 }
