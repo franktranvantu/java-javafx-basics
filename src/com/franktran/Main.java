aa389170104929fa050c29cf611712efd13a5c0f
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-02-26 15:33:34 +0700

    Update

--- src/com/franktran/Main.java
+++ src/com/franktran/Main.java
@@ -3,9 +3,17 @@ package com.franktran;
 import javafx.application.Application;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -18,19 +26,19 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         stage.setTitle("Properties");
 
-        IntegerProperty x = new SimpleIntegerProperty(3);
-        IntegerProperty y = new SimpleIntegerProperty();
+        Label label1 = new Label("Type into below, I will be changed");
+        Label label2 = new Label();
+        TextField input = new TextField();
 
-        y.bind(x.multiply(10));
+        HBox hBox = new HBox(label1, label2);
+        hBox.setAlignment(Pos.CENTER);
 
-        Button button = new Button("Click me!");
-        button.setOnAction(actionEvent -> {
-            x.setValue(5);
-            System.out.println(x.getValue());
-            System.out.println(y.getValue());
-        });
+        VBox layout = new VBox(20);
+        layout.setPadding(new Insets(10, 10, 10, 10));
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(input, hBox);
 
-        StackPane layout = new StackPane(button);
+        label2.textProperty().bind(input.textProperty());
 
         Scene scene = new Scene(layout, 300, 250);
 
