2110dc37ce71daa6ad73f91fec22bbc1a63a22a0
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-02-26 10:51:37 +0700

    Update

--- src/com/franktran/Main.java
+++ src/com/franktran/Main.java
@@ -1,12 +1,11 @@
 package com.franktran;
 
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -22,23 +21,44 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         window = stage;
 
-        ListView<String> listView = new ListView<>();
-        listView.getItems().addAll("HTML", "CSS", "JAVASCRIPT", "JAVA", "PYTHON");
-        listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        TreeItem<String> root = new TreeItem<>();
+        root.setExpanded(true);
 
-        Button button = new Button("Click me");
-        button.setOnAction(actionEvent -> {
-            ObservableList<String> programs = listView.getSelectionModel().getSelectedItems();
-            programs.forEach(System.out::println);
+        TreeItem<String> out = new TreeItem<>("out");
+        out.setExpanded(true);
+        root.getChildren().add(out);
+
+        TreeItem<String> production = new TreeItem<>("production");
+        production.setExpanded(true);
+        out.getChildren().add(production);
+
+        TreeItem<String> src = new TreeItem<>("src");
+        src.setExpanded(true);
+        root.getChildren().add(src);
+
+        TreeItem<String> pack = new TreeItem<>("com.franktran");
+        pack.setExpanded(true);
+        src.getChildren().add(pack);
+
+        TreeItem<String> main = new TreeItem<>("Main");
+        main.setExpanded(true);
+        pack.getChildren().add(main);
+
+        TreeView<String> treeView = new TreeView<>(root);
+        treeView.setShowRoot(false);
+        treeView.getSelectionModel().selectedItemProperty().addListener((value, oldValue, newValue) -> {
+            if (newValue != null) {
+                System.out.println(newValue.getValue());
+            }
         });
 
         VBox layout = new VBox(20);
         layout.setAlignment(Pos.CENTER);
-        layout.getChildren().addAll(listView, button);
+        layout.getChildren().addAll(treeView);
 
         Scene scene = new Scene(layout, 300, 200);
 
-        window.setTitle("ListView");
+        window.setTitle("TreeView");
         window.setScene(scene);
         window.show();
     }
